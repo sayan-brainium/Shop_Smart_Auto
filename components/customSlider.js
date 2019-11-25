c5574638c2d3216ceab18bcc0a3e43974c0d3b6f
sayan.brainium <sayanbanerjee.brainium@gmail.com>
2019-11-25 19:46:38 +0530

    payment calculator

--- components/customSlider.js
+++ components/customSlider.js
@@ -1,55 +1,91 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+// import React from 'react'
+// import {View, Text, StyleSheet} from 'react-native'
 
-export default () => {
+// export default () => {
 
-  return (
-      <View>
-        <View style={style.layer}>
-         <View style={style.rod}>
-           <View style={style.progressBar}></View>
-         </View>
-        </View>
-        <View style={{position:'absolute',top:-3,left:'30%'}}>
-        <View style={style.outercircle}>
-        <View style={style.innercircle}></View>
-        </View>
-        </View>
+//   return (
+//       <View>
+//         <View style={style.layer}>
+//          <View style={style.rod}>
+//            <View style={style.progressBar}></View>
+//          </View>
+//         </View>
+//         <View style={{position:'absolute',top:-3,left:'30%'}}>
+//         <View style={style.outercircle}>
+//         <View style={style.innercircle}></View>
+//         </View>
+//         </View>
+//       </View>
+//     )
+// }
+
+// const style = StyleSheet.create({
+//   rod: {
+//     width: '100%',
+//     borderRadius: 10,
+//     height: 7,
+//     backgroundColor: '#dcdcdc'
+//   },
+//   layer: {
+//     borderRadius: 10,
+//     width: '100%',
+//     padding: 5,
+//     backgroundColor: '#f9f8f6'
+//   },
+//   progressBar: {
+//     width: '30%',
+//     backgroundColor: '#64ca42',
+//     height: 6,
+//     borderRadius: 10
+//   },
+//   outercircle:{
+//     justifyContent:'center',
+//     backgroundColor:'#ffffff',
+//     width:22,
+//     height:22,
+//     borderRadius:11
+//   },
+//   innercircle:{
+//     alignSelf:'center',
+//     backgroundColor:'#64ca42',
+//     width:14,
+//     height:14,
+//     borderRadius:7
+//   }
+// })
+
+import React from 'react';
+import { StyleSheet, Image,View,Text } from 'react-native';
+
+class CustomMarker extends React.Component {
+  render() {
+    return (
+      <View style={styles.Container}>
+         <View style={styles.subContainer}/>
       </View>
-    )
+    );
+  }
 }
 
-const style = StyleSheet.create({
-  rod: {
-    width: '100%',
-    borderRadius: 10,
-    height: 7,
-    backgroundColor: '#dcdcdc'
+const styles = StyleSheet.create({
+  Container: {
+    backgroundColor:"#f9f8f6",
+    width:20,
+    height:20,
+   borderRadius:10,
+   alignContent:"center",
+   alignItems:"center",
+   justifyContent:"center"
   },
-  layer: {
-    borderRadius: 10,
-    width: '100%',
-    padding: 5,
-    backgroundColor: '#f9f8f6'
-  },
-  progressBar: {
-    width: '30%',
-    backgroundColor: '#64ca42',
-    height: 6,
-    borderRadius: 10
-  },
-  outercircle:{
-    justifyContent:'center',
-    backgroundColor:'#ffffff',
-    width:22,
-    height:22,
-    borderRadius:11
-  },
-  innercircle:{
-    alignSelf:'center',
-    backgroundColor:'#64ca42',
-    width:14,
-    height:14,
-    borderRadius:7
+  subContainer:{
+    backgroundColor:"#64ca42",
+    width:12,
+    height:12,
+   borderRadius:6,
+   alignContent:"center",
+   alignItems:"center"
   }
-})
+});
+
+export default CustomMarker;
+
--- screens/PaymentCalculator.js
+++ screens/PaymentCalculator.js
@@ -1,70 +1,143 @@
-import React, {useState, useEffect} from 'react';
-import { ScrollView, TextInput,StyleSheet,ProgressBarAndroid,Text,ImageBackground,TouchableOpacity,StatusBar,View ,Platform} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, TextInput, StyleSheet, ProgressBarAndroid, Text, ImageBackground, TouchableOpacity, StatusBar, View, Platform } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import Icon from 'react-native-vector-icons/Ionicons';
 // import { TextInput } from 'react-native-gesture-handler';
+import MultiSlider from '@ptomasroos/react-native-multi-slider';
 import { LinearGradient } from 'expo-linear-gradient';
-import CustomSlider from './../components/customSlider'
+import CustomMarker from './../components/customSlider'
 
 export default function PaymentCalculator() {
-  // const [value, onChangeText] = useState('');
-  // const [value1, onChangeText1] = useState('');
-  // const [value2, onChangeText2] = useState('');
-  // const [value3, onChangeText3] = useState('');
-  // const [value4, onChangeText4] = useState('');
+  const [sliderOneChanging, setSliderOneChanging] = useState(false);
+  const [sliderTwoChanging, setSliderTwoChanging] = useState(false);
+  const [sliderThreeChanging, setSliderThreeChanging] = useState(false);
+  const [sliderFourChanging, setSliderFourChanging] = useState(false);
+  const [sliderOneValue, setSliderOneValue] = useState([0]);
+  const [sliderTwoValue, setSliderTwoValue] = useState([0]);
+  const [sliderThreeValue, setSliderThreeValue] = useState([0]);
+  const [sliderFourValue, setSliderFourValue] = useState([0]);
+
+  sliderOneValuesChangeStart = () => {
+    setSliderOneChanging(true);
+  };
+
+  sliderTwoValuesChangeStart = () => {
+    setSliderTwoChanging(true);
+  };
+
+  sliderThreeValuesChangeStart = () => {
+      setSliderThreeChanging(true);
+  };
+  sliderFourValuesChangeStart = () => {
+    setSliderFourChanging(true);
+};
+
+  sliderOneValuesChange = values => {
+    console.log("hellotune:", values)
+    let newValues = [0];
+    newValues[0] = values[0];
+    setSliderOneValue({
+      sliderOneValue: newValues,
+    });
+  };
+
+  sliderTwoValuesChange = values => {
+    let newValues = [0];
+    newValues[0] = values[0];
+    setSliderTwoValue({
+      sliderTwoValue: newValues,
+    });
+  };
+
+  sliderThreeValuesChange = values => {
+    let newValues = [0];
+    newValues[0] = values[0];
+    setSliderThreeValue({
+      sliderThreeValue: newValues,
+    });
+  };
+  sliderFourValuesChange = values => {
+    let newValues = [0];
+    newValues[0] = values[0];
+    setSliderFourValue({
+      sliderFourValue: newValues,
+    });
+  };
+
+  sliderOneValuesChangeFinish = () => {
+    setSliderOneChanging(false);
+  };
+
+  sliderTwoValuesChangeFinish = () => {
+    setSliderTwoChanging(false);
+  };
+
+  sliderThreeValuesChangeFinish = () => {
+    setSliderThreeChanging(false);
+  };
+  sliderFourValuesChangeFinish = () => {
+    setSliderFourChanging(false);
+  };
   return (
     <>
-    
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      {/* <ExpoLinksView /> */}
-      
-     <View style={{marginHorizontal:40,marginBottom:50,marginTop:30}}>
-     <Text style={{marginBottom:8,fontSize:14,color:'#535353'}}>APR</Text>
-     {/* <ProgressBarAndroid
-          styleAttr="Horizontal"
-          indeterminate={false}
-          color='#78cb28'
-          progress={0.25}
-        /> */}
-       <CustomSlider />
+
+      <ScrollView style={styles.container}>
+
+        <View style={{ marginHorizontal: 40, marginBottom: 20, marginTop: 30 }}>
+          <Text style={{ marginBottom: 8, fontSize: 14, color: '#535353' }}>APR</Text>
+          <View style={{ alignItems: "center", justifyContent: "center", marginTop: -8 }}>
+            <MultiSlider
+              values={sliderOneValue}
+              sliderLength={300}
+              onValuesChangeStart={sliderOneValuesChangeStart}
+              onValuesChange={sliderOneValuesChange}
+              onValuesChangeFinish={sliderOneValuesChangeFinish}
+              customMarker={CustomMarker}
+            />
+          </View>
+        </View>
+        <View style={{ marginHorizontal: 40, marginBottom: 20 }}>
+          <Text style={{ marginBottom: 8, fontSize: 14, color: '#535353' }}>Term</Text>
+          <View style={{ alignItems: "center", justifyContent: "center", marginTop: -8 }}>
+            <MultiSlider
+              values={sliderTwoValue}
+              sliderLength={300}
+              onValuesChangeStart={sliderTwoValuesChangeStart}
+              onValuesChange={sliderTwoValuesChange}
+              onValuesChangeFinish={sliderTwoValuesChangeFinish}
+              customMarker={CustomMarker}
+            />
+          </View>
+        </View>
+        <View style={{ marginHorizontal: 40, marginBottom: 20 }}>
+          <Text style={{ marginBottom: 8, fontSize: 14, color: '#535353' }}>Down Payment</Text>
+           <View style={{ alignItems: "center", justifyContent: "center", marginTop: -8 }}>
+          <MultiSlider
+            values={sliderThreeValue}
+            sliderLength={300}
+            onValuesChangeStart={sliderThreeValuesChangeStart}
+            onValuesChange={sliderThreeValuesChange}
+            onValuesChangeFinish={sliderThreeValuesChangeFinish}
+            customMarker={CustomMarker}
+          />
+          </View>
+        </View>
+      <View style={{ marginHorizontal: 40, marginBottom:20 }}>
+        <Text style={{ marginBottom: 8, fontSize: 14, color: '#535353' }}>Trade in</Text>
+        <View style={{ alignItems: "center", justifyContent: "center", marginTop: -8 }}>
+          <MultiSlider
+            values={sliderFourValue}
+            sliderLength={300}
+            onValuesChangeStart={sliderFourValuesChangeStart}
+            onValuesChange={sliderFourValuesChange}
+            onValuesChangeFinish={sliderFourValuesChangeFinish}
+            customMarker={CustomMarker}
+          />
+          </View>
       </View>
-      <View style={{marginHorizontal:40,marginBottom:50}}>
-     <Text style={{marginBottom:8,fontSize:14,color:'#535353'}}>Term</Text>
-      {/* <ProgressBarAndroid
-          styleAttr="Horizontal"
-          indeterminate={false}
-          color='#78cb28'
-          progress={0.4}
-        /> */}
-        <CustomSlider />
-      </View>
-      <View style={{marginHorizontal:40,marginBottom:50}}>
-     <Text style={{marginBottom:8,fontSize:14,color:'#535353'}}>Down Payment</Text>
-      {/* <ProgressBarAndroid
-          styleAttr="Horizontal"
-          indeterminate={false}
-          color='#78cb28'
-          progress={0.75}
-        /> */}
-        <CustomSlider />
-      </View>
-      <View style={{marginHorizontal:40,marginBottom:50}}>
-     <Text style={{marginBottom:8,fontSize:14,color:'#535353'}}>Trade in</Text>
-      {/* <ProgressBarAndroid
-          styleAttr="Horizontal"
-          indeterminate={false}
-          color='#78cb28'
-          progress={0.5}
-        /> */}
-        <CustomSlider />
-      </View>
-      <View style={{marginBottom:20,alignSelf:'center'}}>
-        <TouchableOpacity style={{height:65,width:170,borderWidth:1,borderColor:'#e4e4e4',borderRadius:5,justifyContent:'center'}}>
-          <Text style={{alignSelf:'center',fontSize:16,color:'#535353'}}>Payment : $298</Text>
+      <View style={{ marginBottom: 20, alignSelf: 'center' }}>
+        <TouchableOpacity style={{ height: 65, width: 170, borderWidth: 1, borderColor: '#e4e4e4', borderRadius: 5, justifyContent: 'center' }}>
+          <Text style={{ alignSelf: 'center', fontSize: 16, color: '#535353' }}>Payment : $298</Text>
         </TouchableOpacity>
       </View>
 
@@ -76,49 +149,49 @@ export default function PaymentCalculator() {
 PaymentCalculator.navigationOptions = ({ navigation }) => ({
   headerTitle: (
     'Payment Calculator'
- ),
+  ),
 
- headerTintColor: '#fff',
- headerStyle: {
-   backgroundColor: '#0e3ba0',
-   fontSize:'18',
-   fontWeight: 'bold',
-   // marginTop: -10,
-   // height:50
-   height: Platform.OS === 'android' ? 30 : 55,
-     marginTop: Platform.OS === 'ios' ? -10 : 0,
-     paddingBottom: Platform.OS === 'ios' ? 0: 25,
-     ...Platform.select({
-       ios: { backgroundColor: '#0e3ba0'},
-       android: { backgroundColor: '#0e3ba0'}
-     }),
- },
- // headerLeft: (
- //   <TouchableOpacity>
- //     <ImageBackground
- //           style={{ width: 20, height: 20,marginTop:3,position:'relative',color: '#fff' }}
- //           source={require('../assets/images/back_icon.png')}>
- //         </ImageBackground>
- //     {/* <Icon name="ios-menu" style={{ paddingLeft: 20 }} size={25} color="#fff" /> */}
- //   </TouchableOpacity>
- // ),
- headerRight:(
-   <View style={{ flexDirection: 'row' }}>
-      
+  headerTintColor: '#fff',
+  headerStyle: {
+    backgroundColor: '#0e3ba0',
+    fontSize: '18',
+    fontWeight: 'bold',
+    // marginTop: -10,
+    // height:50
+    height: Platform.OS === 'android' ? 30 : 55,
+    marginTop: Platform.OS === 'ios' ? -10 : 0,
+    paddingBottom: Platform.OS === 'ios' ? 0 : 25,
+    ...Platform.select({
+      ios: { backgroundColor: '#0e3ba0' },
+      android: { backgroundColor: '#0e3ba0' }
+    }),
+  },
+  // headerLeft: (
+  //   <TouchableOpacity>
+  //     <ImageBackground
+  //           style={{ width: 20, height: 20,marginTop:3,position:'relative',color: '#fff' }}
+  //           source={require('../assets/images/back_icon.png')}>
+  //         </ImageBackground>
+  //     {/* <Icon name="ios-menu" style={{ paddingLeft: 20 }} size={25} color="#fff" /> */}
+  //   </TouchableOpacity>
+  // ),
+  headerRight: (
+    <View style={{ flexDirection: 'row' }}>
+
       <TouchableOpacity>
-         <ImageBackground
-           style={{ width: 25, height: 20,marginTop:3,position:'relative',marginRight:20, color: '#fff' }}
-           source={require('../assets/images/bell_icon.png')}>
-         </ImageBackground>
-       </TouchableOpacity>
-      
-       <TouchableOpacity  onPress={() => navigation.navigate('HeaderSearch')}>
-         <Icon name="ios-search" style={{ paddingRight: 20 }} size={25} color="#fff" />
-       </TouchableOpacity>
-     
-     </View>
- ),
-  
+        <ImageBackground
+          style={{ width: 25, height: 20, marginTop: 3, position: 'relative', marginRight: 20, color: '#fff' }}
+          source={require('../assets/images/bell_icon.png')}>
+        </ImageBackground>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate('HeaderSearch')}>
+        <Icon name="ios-search" style={{ paddingRight: 20 }} size={25} color="#fff" />
+      </TouchableOpacity>
+
+    </View>
+  ),
+
 });
 
 const styles = StyleSheet.create({
@@ -126,6 +199,21 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  sliders: {
+    margin: 10,
+    width: 280,
+  },
+  text: {
+    alignSelf: 'center',
+    paddingVertical: 20,
+  },
+  title: {
+    fontSize: 30,
+  },
+  sliderOne: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 
 });
